f434cb907ee6d4ca25ecbab7cc245ba882106728
luiz <lguilhermefl@gmail.com>
2023-01-18 19:24:41 -0300

    feat: create header

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
-import "./styles/global.css";
+import './styles/global.css';
 
-import { Habit } from "./components/Habit";
+import logoImage from './assets/logo.svg';
 
-function App() {
-  return <Habit />;
+export function App() {
+  return (
+    <div className='w-screen h-screen flex justify-center items-center'>
+      <div className='w-full max-w-5xl px-6 flex flex-col gap-16'>
+        <div className='w-full max-w-3xl mx-auto flex items-center justify-between'>
+          <img src={logoImage} alt='Habits' />
+          <button
+            type='button'
+            className='border border-violet-500 font-semibold rounded-lg px-6 py-4'
+          >
+            Novo hábito
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
