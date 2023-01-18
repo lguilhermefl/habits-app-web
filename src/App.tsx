d6e88a7371b5864fa57f7f6dd4b7d97ccab6c300
luiz <lguilhermefl@gmail.com>
2023-01-18 19:29:33 -0300

    feat: create header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,8 @@
 import './styles/global.css';
-
-import logoImage from './assets/logo.svg';
+import { Header } from './components/Header';
 
 export function App() {
-  return (
-    <div className='w-screen h-screen flex justify-center items-center'>
-      <div className='w-full max-w-5xl px-6 flex flex-col gap-16'>
-        <div className='w-full max-w-3xl mx-auto flex items-center justify-between'>
-          <img src={logoImage} alt='Habits' />
-          <button
-            type='button'
-            className='border border-violet-500 font-semibold rounded-lg px-6 py-4'
-          >
-            Novo hábito
-          </button>
-        </div>
-      </div>
-    </div>
-  );
+  return <Header />;
 }
 
 export default App;
