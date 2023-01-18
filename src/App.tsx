50eff55cbe4baac08a41c157d3779bdf8f4e28c3
luiz <lguilhermefl@gmail.com>
2023-01-18 19:31:59 -0300

    fix: move container divs from header to app component

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,13 @@ import './styles/global.css';
 import { Header } from './components/Header';
 
 export function App() {
-  return <Header />;
+  return (
+    <div className='w-screen h-screen flex justify-center items-center'>
+      <div className='w-full max-w-5xl px-6 flex flex-col gap-16'>
+        <Header />
+      </div>
+    </div>
+  );
 }
 
 export default App;
