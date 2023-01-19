1bf48b420283bd40d86d064bc72d6f6781cebc3e
luiz <lguilhermefl@gmail.com>
2023-01-19 12:23:01 -0300

    feat: add modal progress bar

--- src/components/HabitDay.tsx
+++ src/components/HabitDay.tsx
@@ -1,4 +1,5 @@
 import * as Popover from '@radix-ui/react-popover';
+import clsx from 'clsx';
 import { ProgressBar } from './ProgressBar';
 
 interface HabitDayProps {
@@ -10,7 +11,20 @@ export function HabitDay({ completed, amount }: HabitDayProps) {
   const completedPercentage = Math.round((completed / amount) * 100);
   return (
     <Popover.Root>
-      <Popover.Trigger className='w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg' />
+      <Popover.Trigger
+        className={clsx('w-10 h-10 border-2 rounded-lg', {
+          'bg-zinc-900 border-zinc-800': completedPercentage === 0,
+          'bg-violet-900 border-violet-700':
+            completedPercentage > 0 && completedPercentage < 20,
+          'bg-violet-800 border-violet-600':
+            completedPercentage >= 20 && completedPercentage < 40,
+          'bg-violet-700 border-violet-500':
+            completedPercentage >= 40 && completedPercentage < 60,
+          'bg-violet-600 border-violet-500':
+            completedPercentage >= 60 && completedPercentage < 80,
+          'bg-violet-500 border-violet-400': completedPercentage >= 80,
+        })}
+      />
       <Popover.Portal>
         <Popover.Content className='min-w-[320px] p-6 rounded-2xl bg-zinc-900 flex flex-col'>
           <span className='font-semibold text-zinc-400'>quinta-feira</span>
