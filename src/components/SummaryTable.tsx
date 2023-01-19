c2b9f57bb98314c3f98d00e7488aaa18c1edf186
luiz <lguilhermefl@gmail.com>
2023-01-19 12:23:26 -0300

    feat: add habit day props

--- src/components/SummaryTable.tsx
+++ src/components/SummaryTable.tsx
@@ -23,7 +23,11 @@ export function SummaryTable() {
       </div>
       <div className='grid grid-rows-7 grid-flow-col gap-3'>
         {summaryDates.map((date) => (
-          <HabitDay key={date.toString()} />
+          <HabitDay
+            key={date.toString()}
+            amount={5}
+            completed={Math.round(Math.random() * 5)}
+          />
         ))}
         {amountOdDaysToFill > 0 &&
           Array.from({ length: amountOdDaysToFill }).map((_, i) => (
