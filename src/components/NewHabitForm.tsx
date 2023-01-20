aff2c426d775a40412065e10ab317e0adbd96dd1
luiz <lguilhermefl@gmail.com>
2023-01-19 21:51:21 -0300

    feat: add week days checkbox

--- src/components/NewHabitForm.tsx
+++ src/components/NewHabitForm.tsx
@@ -1,4 +1,15 @@
+import * as Checkbox from '@radix-ui/react-checkbox';
 import { Check } from 'phosphor-react';
+
+const availableWeekDays = [
+  'Domingo',
+  'Segunda-feira',
+  'Terça-feira',
+  'Quarta-feira',
+  'Quinta-feira',
+  'Sexta-feira',
+  'Sábado',
+];
 
 export function NewHabitForm() {
   return (
@@ -18,6 +29,25 @@ export function NewHabitForm() {
       <label htmlFor='' className='font-semibold leading-tight mt-4'>
         Qual a recorrência?
       </label>
+
+      <div className='flex flex-col gap-2 mt-3'>
+        {availableWeekDays.map((weekDay) => {
+          return (
+            <Checkbox.Root
+              key={weekDay}
+              className='flex items-center gap-3 group'
+            >
+              <div className='h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500'>
+                <Checkbox.Indicator>
+                  <Check size={20} className='text-white' />
+                </Checkbox.Indicator>
+              </div>
+
+              <span className='text-white leading-tight'>{weekDay}</span>
+            </Checkbox.Root>
+          );
+        })}
+      </div>
 
       <button
         type='submit'
