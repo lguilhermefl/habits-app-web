0b528a71553b70ffb2dfe8ddaa40f93fe924ae02
luiz <lguilhermefl@gmail.com>
2023-01-20 19:52:09 -0300

    feat: get day habits

--- src/components/HabitsList.tsx
+++ src/components/HabitsList.tsx
@@ -1,26 +1,55 @@
 import * as Checkbox from '@radix-ui/react-checkbox';
 import { Check } from 'phosphor-react';
+import { useEffect, useState } from 'react';
+import { api } from '../lib/axios';
 
 interface HabitsListProps {
   date: Date;
 }
 
-export function HabitsList({ date }: HabitsListProps) {
-  return (
-    <>
-      <div className='mt-6 flex flex-col gap-3'>
-        <Checkbox.Root className='flex items-center gap-3 group'>
-          <div className='h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500'>
-            <Checkbox.Indicator>
-              <Check size={20} className='text-white' />
-            </Checkbox.Indicator>
-          </div>
+interface HabitsInfo {
+  possibleHabits: {
+    id: string;
+    title: string;
+    created_at: string;
+  }[];
+  completedHabits: string[];
+}
 
-          <span className='font-semibold text-xl text-white leading-tight group-data-[state=checked]:line-through group-data-[state=checked]:text-zinc-400'>
-            Beber 2L de água
-          </span>
-        </Checkbox.Root>
-      </div>
-    </>
+export function HabitsList({ date }: HabitsListProps) {
+  const [habitsInfo, setHabitsInfo] = useState<HabitsInfo>();
+
+  useEffect(() => {
+    api
+      .get('day', {
+        params: {
+          date: date.toISOString(),
+        },
+      })
+      .then((response) => setHabitsInfo(response.data));
+  }, []);
+
+  return (
+    <div className='mt-6 flex flex-col gap-3'>
+      {habitsInfo?.possibleHabits.map((habit) => {
+        return (
+          <Checkbox.Root
+            key={habit.id}
+            checked={habitsInfo.completedHabits.includes(habit.id)}
+            className='flex items-center gap-3 group'
+          >
+            <div className='h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500'>
+              <Checkbox.Indicator>
+                <Check size={20} className='text-white' />
+              </Checkbox.Indicator>
+            </div>
+
+            <span className='font-semibold text-xl text-white leading-tight group-data-[state=checked]:line-through group-data-[state=checked]:text-zinc-400'>
+              {habit.title}
+            </span>
+          </Checkbox.Root>
+        );
+      })}
+    </div>
   );
 }
